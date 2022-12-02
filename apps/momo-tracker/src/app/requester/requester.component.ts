3f3603f6594f91dc489c07dc51a72302880fba32
Marc <marc@docorga.com>
2022-12-02 16:13:44 +0100

    [MT] Pimp requester view

--- apps/momo-tracker/src/app/requester/requester.component.ts
+++ apps/momo-tracker/src/app/requester/requester.component.ts
@@ -8,12 +8,37 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class RequesterComponent implements OnInit {
   searchForm!: FormGroup;
+  time!: Date;
+  hours!: number;
+  msg!: string;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+    setInterval(() => {
+        this.time = new Date();
+    }, 1000);
+
+    this.decide();
+  }
 
   ngOnInit(): void {
     this.searchForm = this.fb.group({
       search: [''],
     });
+  }
+
+  getCurrentDate() {
+    setInterval(() => {
+    this.time = new Date();
+   }, 1000);
+  }
+
+  decide() {
+    this.hours = new Date().getHours();
+    if(this.hours < 18) {
+      this.msg = "Bonjour"
+    }
+    else {
+      this.msg = "Bonsoir"
+    }
   }
 }
